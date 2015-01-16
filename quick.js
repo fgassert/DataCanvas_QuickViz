885b32c346f5cf659e4af01a53f5e01b310ea895
fgassert <cowbox314@gmail.com>
2015-01-16 17:40:29 -0500

    alldata

--- quick.js
+++ quick.js
@@ -9,37 +9,90 @@ var chart = (function() {
 
     var loadData = function(err, res) {
 	var obj = JSON.parse(res.responseText)
-	var data = {labels:[],datasets:[{label:'Dust',
+	var data = {labels:[],datasets:[
+		{label:'Dust',
+		fillColor: "rgba(180,180,180,0.2)",
+		strokeColor: "rgba(180,180,180,1)",
+		pointColor: "rgba(180,180,180,1)",
+		pointStrokeColor: "#fff",
+		pointHighlightFill: "#fff",
+		pointHighlightStroke: "rgba(220,220,220,1)",
+		data: []},
+		{label:'Temperature',
 		fillColor: "rgba(220,220,220,0.2)",
 		strokeColor: "rgba(220,220,220,1)",
 		pointColor: "rgba(220,220,220,1)",
 		pointStrokeColor: "#fff",
 		pointHighlightFill: "#fff",
 		pointHighlightStroke: "rgba(220,220,220,1)",
-		data: []
-	}]}
+		data: []},
+		{label:'Humidity',
+		fillColor: "rgba(180,180,220,0.2)",
+		strokeColor: "rgba(180,180,220,1)",
+		pointColor: "rgba(180,180,220,1)",
+		pointStrokeColor: "#fff",
+		pointHighlightFill: "#fff",
+		pointHighlightStroke: "rgba(220,220,220,1)",
+		data: []},
+		{label:'Sound',
+		fillColor: "rgba(180,200,190,0.2)",
+		strokeColor: "rgba(180,200,190,1)",
+		pointColor: "rgba(180,200,190,1)",
+		pointStrokeColor: "#fff",
+		pointHighlightFill: "#fff",
+		pointHighlightStroke: "rgba(220,220,220,1)",
+		data: []},
+		{label:'UV',
+		fillColor: "rgba(210,180,220,0.2)",
+		strokeColor: "rgba(210,180,220,1)",
+		pointColor: "rgba(210,180,220,1)",
+		pointStrokeColor: "#fff",
+		pointHighlightFill: "#fff",
+		pointHighlightStroke: "rgba(220,220,220,1)",
+		data: []},
+		{label:'Light',
+		fillColor: "rgba(220,220,180,0.2)",
+		strokeColor: "rgba(220,220,180,1)",
+		pointColor: "rgba(220,220,180,1)",
+		pointStrokeColor: "#fff",
+		pointHighlightFill: "#fff",
+		pointHighlightStroke: "rgba(220,220,220,1)",
+		data: []}]};
 	for (var i=0; i<obj.length; i++) {
 	    var d = Date.parse(obj[obj.length-i-1].timestamp);
 	    data.labels[i] = new Date(d).toLocaleTimeString();
 	    data.datasets[0].data[i] = obj[obj.length-i-1].data.dust;
+	    data.datasets[1].data[i] = obj[obj.length-i-1].data.temperature;
+	    data.datasets[2].data[i] = obj[obj.length-i-1].data.humidity;
+	    data.datasets[3].data[i] = obj[obj.length-i-1].data.sound/10;
+	    data.datasets[4].data[i] = obj[obj.length-i-1].data.uv;
+	    data.datasets[5].data[i] = obj[obj.length-i-1].data.light;
 	};
 	if (c === undefined) {
-	    c = new Chart(ctx).Line(data,{responsive:true,scaleShowGridLines:false});
+	    c = new Chart(ctx).Line(data,{responsive:true,
+					  scaleShowGridLines:false,
+					  legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%=datasets[i].label%></li><%}%></ul>"
+});
 	    console.log('newchart')
+	    var legend = document.getElementById('legend');
+	    legend.innerHTML = c.generateLegend()
 	} else {
 	    for (var i=0; i<data.labels.length; i++) {
 		if (c.scale.xLabels.indexOf(data.labels[i])==-1) {
-		    c.addData([data.datasets[0].data[i]], data.labels[i]);
+		    c.addData([data.datasets[0].data[i],
+			      data.datasets[1].data[i],
+			      data.datasets[2].data[i],
+			      data.datasets[3].data[i],
+			      data.datasets[4].data[i],
+			      data.datasets[5].data[i]
+			      ], data.labels[i]);
 		};
 	    };
 	    if (c.scale.xLabels.length > maxLen) {
 		var r = c.scale.xLabels.length-maxLen;
 		for (var i=0; i<r; i++) {
-		    c.scale.xLabels.shift();
-		    c.datasets[0].points.shift();
+		    c.removeData();
 		};
-		c.scale.valuesCount = maxLen;
-		c.update();
 	    };
 	};
 	console.log(c);
@@ -50,5 +103,8 @@ var chart = (function() {
 	window.setTimeout(update, 10000);
     };
     window.setTimeout(update, 10000);
+
+    
     return c;
+    
 })()
